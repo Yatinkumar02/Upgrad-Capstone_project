ed87d168d03471d7f5c1f2aa25ee728fe54121a7
Yatinkumar02 <2002yatinkumar@gmail.com>
2023-08-10 12:30:06 +0530

    Added const port 3000

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const server = http.createServer((request, response) =>{
+    console.log('method: ${request.method}');
+    console.log('url: ${request.url}');
+    
+    if(request.url === '/movies'){
+        if(request.method === 'GET'){
+            response.write( "All Movies Data in JSON format from Mongo DB")
+        }
+    }
+
+    if(request.url === '/genres'){
+        if(request.method === 'GET'){
+            response.write( "All Genres Data in JSON format from Mongo DB")
+        }
+    }
+
+    if(request.url === '/artists'){
+        if(request.method === 'GET'){
+            response.write( "All Artist Data in JSON format from Mongo DB")
+        }
+    }
+
+    response.end();
+});
+
+const PORT = 3000;
+server.listen(3000, () =>{
+    console.log('server started on port 3000');
+});
